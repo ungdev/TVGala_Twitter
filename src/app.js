1121dcf1253dcd2bdf6d00e05dbfba681f51567b
Arnaud9145 <arnaud.dufour@utt.fr>
2018-11-11 14:16:13 +0100

    added censor

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import Twitter     from 'twitter';
 import request     from 'request';
+import removeAccents from 'remove-accents'
 require('dotenv').config()
 
 const client = new Twitter({
@@ -14,14 +15,31 @@ const stream = client.stream('statuses/filter', { track: `#${process.env.HASHTAG
 stream.on('data', function(event) {
     console.log("tweet !")
     //console.log(event.text)
-    request.post({
-        url    : `${process.env.SERVER_URL}/sms`,
-        headers: { 'content-type': 'application/json' },
-        body   : JSON.stringify({
-            message: event.text,
-            from   : event.user.screen_name
-        })
-    });
+    const censored = process.env.CENSORED
+    let found = censored ? censored.split(',')
+      .find(word => removeAccents(event.text)
+        .toLowerCase()
+        .includes(removeAccents(word).toLowerCase()))
+    : false
+    if(found) {
+      request.post({
+          url    : `${process.env.SLACK_URL}`,
+          headers: { 'content-type': 'application/json' },
+          body   : JSON.stringify({
+              text: `Message censuré en provencance de ${event.user.screen_name}: "${event.text}"`,
+          })
+      });
+    }
+    else {
+      request.post({
+          url    : `${process.env.SERVER_URL}/sms`,
+          headers: { 'content-type': 'application/json' },
+          body   : JSON.stringify({
+              message: event.text,
+              from   : event.user.screen_name
+          })
+      });
+    }
 });
 
 stream.on('error', function(error) {
